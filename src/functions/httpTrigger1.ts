3587ab92b1bdf1dc54c2fcf27be00cbba9fef015
Keison <keison-tang@users.noreply.github.com>
2023-04-27 00:11:28 +1200

    connect to planetscale

--- src/functions/httpTrigger1.ts
+++ src/functions/httpTrigger1.ts
@@ -5,15 +5,29 @@ import {
   InvocationContext,
 } from '@azure/functions';
 
+import * as mysql from 'mysql2/promise';
+
 export async function httpTrigger1(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
+  context.log(
+    `Http function processed ${request.method} request for url "${request.url}"`
+  );
 
-  const name = request.query.get('name') || (await request.text()) || 'world';
+  try {
+    const connection: mysql.Connection = await mysql.createConnection(
+      process.env['PLANETSCALE_CONNECTION_STRING']
+    );
 
-  return { body: `Hello, ${name}!` };
+    await connection.connect();
+    context.log('Succesfully connected to PlanetScale!');
+
+    return { status: 200 };
+  } catch (error) {
+    context.log(error.stack);
+    return { status: 500, body: error };
+  }
 }
 
 app.http('httpTrigger1', {
